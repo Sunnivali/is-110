ae3288ce503b347734bca0954ffefdb3bdcd8f6c
Sunnivali <sunniva.lien@gmail.com>
2025-01-14 18:05:00 +0100

    Addition-ovelse

---
+++ ch3/Addition/Addition/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+
+class Addition
+{
+    
+    static void Main()
+    {
+    int number1;
+    int number2;
+    int sum;
+    Console.Clear();
+    Console.Write("Enter first integer: ");
+    
+    number1 = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter second integer: ");
+
+    number2 = int.Parse(Console.ReadLine());
+
+    sum = number1 + number2;
+
+    Console.WriteLine($"Sum is {sum}");
+    }
+}
---
+++ ch3/Comparison/Comparison/Program.cs
@@ -0,0 +1,49 @@
+﻿// Comparing integers using if statments, equality operators
+// and relational operators.
+
+using System;
+
+class Comparison
+{
+
+static void Main()
+{
+    Console.Clear();
+    Console.Write("Enter first integer:");
+    int number1 = int.Parse(Console.ReadLine());
+
+
+    Console.Write("Enter second integer:");
+    int number2 = int.Parse(Console.ReadLine());
+
+    if (number1 == number2)
+    {
+        Console.WriteLine($"{number1} == {number2}");
+    }
+
+    if (number1 != number2)
+    {
+        Console.WriteLine($"{number1} != {number2}");
+    }
+
+    if (number1 < number2)
+    {
+        Console.WriteLine($"{number1} < {number2}");
+    }
+
+    if (number1 > number2)
+    {
+        Console.WriteLine($"{number1} > {number2}");
+    }
+
+    if (number1 <= number2)
+    {
+        Console.WriteLine($"{number1} <= {number2}");
+    }
+
+    if (number1 >= number2)
+    {
+        Console.WriteLine($"{number1} >= {number2}");
+    }
+}
+}
