a30f7e176354b41f0e3266937e9b5dd30ed2267c
Sunnivali <sunniva.lien@gmail.com>
2025-03-07 14:12:09 +0100

    kapittel 5 øvelser

--- Modul1/Oppgave1/Handlekurv.cs
+++ Modul1/Oppgave1/Handlekurv.cs
@@ -1,12 +1,11 @@
 using System;
 
-class Handlekurv
+class Handlekurv        // Definerer klassen, med et felt totalpris, som lagre summen av alle varene
 {
     private decimal totalPris = 0; // Holder totalprisen
-
-    public void LeggTilVare(decimal pris)
+    public void LeggTilVare(decimal pris)       // tar inn en parameter av typen decimal, som representere prisen på ein vare
     {
-        totalPris = totalPris + pris; // Legger til prisen i totalen        ein funksjon som legger til varepris til handlekorga sin totalpris
+        totalPris = totalPris + pris; // Legger til prisen i totalen        
     }
 
     public decimal BeregnTotal()
@@ -17,7 +16,7 @@ class Handlekurv
     public void SkrivKvittering()
     {
         Console.WriteLine("\nKvittering:"); // Lager ei tom linje for oversikten sin del
-        Console.WriteLine($"Totalpris: {totalPris} kr"); 
-        Console.Write("\n"); // tom linje
+        Console.WriteLine($"Totalpris: {totalPris} kr");  // Skriver ut totalprisen
+        Console.Write("\n"); // lager en tom linje for lesbarhet
     }
 }
--- Modul1/Oppgave1/Program.cs
+++ Modul1/Oppgave1/Program.cs
@@ -2,23 +2,23 @@
 {
     static void Main()
     {
-        Handlekurv handlekurv = new Handlekurv(); // Lager et objekt handelkurv av klassen Handlekurv
-
-        Console.Clear(); // Fjerner hvor filen er lagret, gjør det mer oversiktlig
-        Console.Write("Hvor mange varer vil du legge til? ");
+        Handlekurv handlekurv = new Handlekurv(); // Lager et objekt av klassen Handlekurv, for å kunne bruke metodene i Handlekurv
         
-        int antallVarer = int.Parse(Console.ReadLine());     // Parse er ein metode i systemklassen int
+        Console.Clear(); 
+        Console.Write("Hvor mange varer vil du legge til? "); // Skriver ute en melding uten å hoppe til ny linje.
+        
+        int antallVarer = int.Parse(Console.ReadLine()); // Lar brukeren skrive inn et tall, Konvertere brukerens input fra teskt til et heltall    
 
-        for (int i = 0; i < antallVarer; i++)
+        for (int i = 0; i < antallVarer; i++) // Løkka kjører like mange ganger som antall varer brukeren skrev inn
         {
-            Console.Write($"Skriv inn prisen på vare {i + 1}: ");
-            decimal pris = decimal.Parse(Console.ReadLine());
-            handlekurv.LeggTilVare(pris);
+            Console.Write($"Skriv inn prisen på vare {i + 1}: "); 
+            decimal pris = decimal.Parse(Console.ReadLine()); // Leser inn brukerens input, og konverterer teksten til et desimaltall
+            handlekurv.LeggTilVare(pris); // Legger prisen til totalprisen, kaller metoden LeggTilVare(pris) i Handlekurv-klassen
         }
 
         handlekurv.SkrivKvittering(); // Skriver ut kvittering
 
-        if (handlekurv.BeregnTotal() > 500)
+        if (handlekurv.BeregnTotal() > 500)  // Henter totalprisen, hvis totalen er mer en 500, får brukeren gratis frakt
         {
             Console.WriteLine("Du får gratis frakt!");
              Console.Write("\n");
